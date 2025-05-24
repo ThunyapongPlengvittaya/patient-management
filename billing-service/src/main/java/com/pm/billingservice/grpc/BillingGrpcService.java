c69b59ef62badf58c1728f44f26784df894eacad
ThunyapongPlengvittaya <thunyapong2544@gmail.com>
2025-05-24 22:10:35 +0700

    billing-service-grpc done

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,15 +1,31 @@
 package com.pm.billingservice.grpc;
 
+import billing.BillingResponse;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
-    //streamObserver คือ รับส่งข้อมูลแบบ realtime
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    //streamObserver คือ รับส่งข้อมูลแบบ realtime ของ grpc
     @Override
     public void createBillingAccount(billing.BillingRequest billingRequest ,
-                                     StreamObserver<billing.BillingResponse> responseStreamObserver) {
+                                     StreamObserver<billing.BillingResponse> responseObserver) {
 
+        log.info("createBillingAccount request received {}", billingRequest.toString());
+
+        // Business logic - e.g save to database, perform calculates etc
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("1234")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -4,6 +4,7 @@ import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -17,9 +18,11 @@ import java.util.stream.Collectors;
 
 @Service
 public class PatientService {
-    private PatientRepository patientRepository;
-    public PatientService(PatientRepository patientRepository) {
+    private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getPatient (){
@@ -39,6 +42,11 @@ public class PatientService {
         }
 
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        billingServiceGrpcClient.createBillingAccount(
+                newPatient.getId().toString(),
+                newPatient.getName(),
+                newPatient.getEmail());
 
         //email must be unique
         return PatientMapper.toDTO(newPatient);
