c0fb09df19612b75323af255113565806841eb16
ThunyapongPlengvittaya <thunyapong2544@gmail.com>
2025-05-29 22:57:56 +0700

    AuthService with API gateway

---
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.pm.authservice.controller;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.dto.LoginResponseDTO;
+import com.pm.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    // ทำหน้าที่เมื่อ receive a request to the login endpoint มันจะ convert request body เป็น DTO object แล้วส่งเข้ามาเป็น argument ใน method นี้
+    // อย่างแรกที่ต้องทำคือ authenticate token ก่อน
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+
+        //invalid
+        if(tokenOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        //get token string and send to constructor(java object)
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+}
---
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -0,0 +1,36 @@
+package com.pm.authservice.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final Key secretKey;
+
+    //แปลง jwt secret key แล้วก็ decode เป็น utf-8
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        byte[] keyBytes = Base64.getDecoder().decode(secret.getBytes(
+                StandardCharsets.UTF_8));
+        //create key token
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role) //custom property
+                .issuedAt(new Date()) // store in key to validate token
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .signWith(secretKey)
+                .compact();
+    }
+}
